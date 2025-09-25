9c408f16ca9f45353abfe7d594466a63a9d24087
Inki Hong <ihong3589@gmail.com>
2025-09-25 20:40:12 +0000

    main.py adds a new icosphere for each bit in the qr code, next step is to give it a new material for true/false values

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+import subprocess
+import sys
+import os
+import bpy
+
+# Path to the Blender Python executable
+# this is for Windows, will need to adjust for other OS
+python_exe = os.path.join(sys.prefix, 'bin', 'python.exe')
+
+# Upgrade pip to the latest version
+subprocess.call([python_exe, "-m", "ensurepip"])
+subprocess.call([python_exe, "-m", "pip", "install", "--upgrade", "pip"])
+
+# Install the desired package
+subprocess.call([python_exe, "-m", "pip", "install", "qrcode"])
+
 import qrcode
 
 # rickroll ppl data
@@ -16,7 +32,29 @@ qr.make(fit=True)
 # Get matrix
 matrix = qr.get_matrix()
 
+curr_y = 0
+curr_x = 0
+
 # Print ASCII QR code
+# origin is at top-left corner
 for row in matrix:
-    line = "".join("██" if col else "  " for col in row)
-    print(line)
+    # for each row, decrement y by 0.1
+    # for each col, increment x by 0.1
+    
+
+    # y = row index
+    # x = column index
+
+    for col in row:
+        # step 1: add icosphere
+        bpy.ops.mesh.primitive_ico_sphere_add(subdivisions=2, radius=0.05, location=(curr_x, curr_y, 0)) 
+        
+        # step 2: increment x by 0.1
+        curr_x += 0.1
+
+    # at the end of each row, decrement y by 0.1 and reset x to 0
+    curr_y -= 0.1
+    curr_x = 0
+
+    # line = "".join("██" if col else "  " for col in row)
+    # print(line)
