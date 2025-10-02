da9f3e00905498d01bfc9ccbd60d13ff82e2d23c
Inki Hong <7340818+inkihong9@users.noreply.github.com>
2025-10-02 13:10:53 -0400

    updated __init__.py to have a single input (data) for encoding into creating qr code, writing some notes and mini documentation on the code snippets

--- qr_code_maker/__init__.py
+++ qr_code_maker/__init__.py
@@ -1,5 +1,6 @@
 import bpy
 
+
 class MESH_OT_add_custom_mesh(bpy.types.Operator):
     """Add a Custom Mesh"""
     bl_idname = "mesh.add_custom_mesh"
@@ -7,32 +8,28 @@ class MESH_OT_add_custom_mesh(bpy.types.Operator):
     bl_options = {'REGISTER', 'UNDO'}
 
     # User input fields (appear in popup)
-    size: bpy.props.FloatProperty(
-        name="Size",
-        default=1.0,
-        min=0.1,
-        max=10.0,
-        description="Size of the mesh"
-    )
-    segments: bpy.props.IntProperty(
-        name="Segments",
-        default=16,
-        min=3,
-        max=128,
-        description="Number of segments"
+    data: bpy.props.StringProperty(
+        name="Data",
+        default="https://github.com/inkihong9",
+        description="Data to encode in the mesh"
     )
 
+    # this function is called when OK is clicked in the popup modal
     def execute(self, context):
+        # get user input
+        data = self.data
+        
         # Example: create a circle mesh with user inputs
         bpy.ops.mesh.primitive_circle_add(
-            radius=self.size,
-            vertices=self.segments,
+            radius=1,
+            vertices=10,
             enter_editmode=False,
             align='WORLD',
             location=(0, 0, 0)
         )
         return {'FINISHED'}
 
+    # this function is called when the operator (Add > Mesh > Custom Mesh) is clicked
     def invoke(self, context, event):
         # This makes the popup appear before execution
         return context.window_manager.invoke_props_dialog(self)
@@ -43,12 +40,12 @@ def menu_func(self, context):
     self.layout.operator(MESH_OT_add_custom_mesh.bl_idname,
                          text="Custom Mesh")
 
-
+# this function is called when the add-on is enabled in Edit > Preferences > Add-ons
 def register():
     bpy.utils.register_class(MESH_OT_add_custom_mesh)
     bpy.types.VIEW3D_MT_mesh_add.append(menu_func)
 
-
+# this function is called when the add-on is disabled in Edit > Preferences > Add-ons
 def unregister():
     bpy.types.VIEW3D_MT_mesh_add.remove(menu_func)
     bpy.utils.unregister_class(MESH_OT_add_custom_mesh)
