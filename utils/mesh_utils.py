6a8dc86bb9cef4f13ef87b816a1493fd5b5a2083
Inki Hong <7340818+inkihong9@users.noreply.github.com>
2025-10-07 01:05:34 -0400

    it builds a scannable QR code successfully, next step is to organize the objects into different collections

--- utils/mesh_utils.py
+++ utils/mesh_utils.py
@@ -11,3 +11,34 @@ def create_stone(name:str, scale:tuple, location:tuple):
     stone.name = name
     stone.scale = scale
     return stone
+
+
+'''
+iterate through the QR code matrix and build the QR code by duplicating the correct stone (black or white) at the correct location
+qr_matrix param is a 2D list of booleans representing the QR code
+white_stone param is the white stone object to duplicate
+black_stone param is the black stone object to duplicate
+'''
+def build_qr_code(qr_matrix, white_stone, black_stone):
+    curr_y = 0
+    curr_x = 0
+
+    for row in qr_matrix:
+        for col in row:
+            stone_copy = None
+            if col:
+                # duplicate black stone
+                stone_copy = black_stone.copy()
+                stone_copy.data = black_stone.data.copy()
+            else:
+                # duplicate white stone
+                stone_copy = white_stone.copy()
+                stone_copy.data = white_stone.data.copy()
+
+            stone_copy.location = (curr_x, curr_y, 0)
+            bpy.context.collection.objects.link(stone_copy)
+            curr_x += 0.1
+
+        # at the end of each row, decrement y by 0.1 and reset x to 0
+        curr_y -= 0.1
+        curr_x = 0
