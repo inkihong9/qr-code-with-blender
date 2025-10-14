63e8526499f35ecaf3c6ffdf30a9b4f1161761a2
Inki Hong <7340818+inkihong9@users.noreply.github.com>
2025-10-13 21:09:55 -0400

    wrote notes on needed variables for building QR code with empty center for putting logos in

--- utils/mesh_utils.py
+++ utils/mesh_utils.py
@@ -72,6 +72,21 @@ def build_qr_code_v2(qr_matrix):
     curr_y = 0
     curr_x = 0
 
+    '''
+    IH (2025-10-13): version 2 algorithm for building QR code with empty center
+    need to compute: 
+    N - size of QR matrix (with border): N = len(qr_matrix)
+    n - size of QR matrix (without border): n = N - (gv.border * 2)
+      - length of QR matrix (n) for all versions of QR code
+    m - size of empty center (without border): m = not sure yet, but should be an odd number
+      - maybe floor(m/3), add 1 if even to make it odd
+      - need to experiment
+    i - start index for empty center
+    j - end index for empty center
+    1. determine the size of the qr_matrix (n x n) - 
+    2. calculate m as follows: m = ?? need to figure out the formula for this
+    '''
+
     # iterate through a row of bits
     for row_of_bits in qr_matrix:
 
