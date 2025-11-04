0089c626cdfbe83f91dfa1e8566e95a0015b5b76
Inki Hong <7340818+inkihong9@users.noreply.github.com>
2025-11-04 11:13:28 -0500

    update the step numbers, and get QR matrix for each input url

--- __init__.py
+++ __init__.py
@@ -64,53 +64,57 @@ class MESH_OT_add_custom_mesh(bpy.types.Operator):
 
     # this function is called when OK is clicked in the popup modal
     def execute(self, context):
-        # step 1. get user input
+        # step 1. initialize and get user input
+        qr_matrices = []
         time_interval = self.time_interval
         flip_time = self.flip_time
         input_urls = [item.value for item in self.urls]
-        input_data = input_urls[0]
+        
 
-        # TODO: step 1.5. do the validations, create a separate user story for this later
+        # TODO: step 2. do the validations on the input urls, create a separate user story for this later
         if input_urls == [''] or len(input_urls) == 0:
             self.report({'ERROR'}, "At least one URL must be provided.")
             return {'CANCELLED'}
 
+        # step 3. get QR code matrix from user input
+        for url in input_urls:
+            qr_matrix = qr_utils.get_qr_matrix(url)
+            qr_matrices.append(qr_matrix)
 
-        # step 2. get QR code matrix from user input
-        qr_matrix = qr_utils.get_qr_matrix(input_data)
 
-        # step 3. create a new collection for storing QR code mesh
+        # step 4. create a new collection for storing QR code mesh
         collection_utils.create_qr_code_collection()
 
-        # step 3. create stone for duplicating throughout the QR code matrix
+        # step 5. create stone for duplicating throughout the QR code matrix
         # future plan: allow the user to choose stone size, shape, scale, how materials are assigned, etc.
         gv.white_stone = mesh_utils.create_stone("white-stone", (1,1,0.3), (-1,1,0))
         gv.black_stone = mesh_utils.create_stone("black-stone", (1,1,0.3), (-1,1.2,0))
 
-        # step 4. create 2 materials: black and white
+        # step 6. create 2 materials: black and white
         # future plan: allow user to choose colors in the popup if possible, white will be limited choices, but non-white color
         #              will be a myriad of colors for high contrast
         white_mat = material_utils.create_material("ivory-white", (1, 0.939, 0.584, 1))
         black_mat = material_utils.create_material("jet-black", (0.002, 0.000607, 0.000911, 1))
 
-        # step 5. assign materials to stones
+        # step 7. assign materials to stones
         gv.white_stone.data.materials.clear()
         gv.white_stone.data.materials.append(white_mat)
         gv.black_stone.data.materials.clear()
         gv.black_stone.data.materials.append(black_mat)
 
-        # step 6. build QR code by duplicating the white and black stones based on the QR code matrix
+        # step 8. build QR code by duplicating the white and black stones based on the QR code matrix
         # mesh_utils.build_qr_code(qr_matrix)
         mesh_utils.build_qr_code_v2(qr_matrix)
 
-        # step 7. hide the original stones from view
+        # step 9. hide the original stones from view
         gv.white_stone.hide_set(True)
         gv.white_stone.hide_render = True
         gv.black_stone.hide_set(True)
         gv.black_stone.hide_render = True
 
-        # step 8. apparently this return value is needed
+        # step 10. apparently this return value is needed
         return {'FINISHED'}
+    
 
     # this function is called when the operator (Add > Mesh > QR Code) is clicked
     def invoke(self, context, event):
